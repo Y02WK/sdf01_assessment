5f44777c09eaa392c77ccdb78fd52ff55834ba5c
Y02WK <59220086+Y02WK@users.noreply.github.com>
2021-12-14 10:48:13 +0800

    Main.java complete.

--- src/main/java/sdf01/nus/Main.java
+++ src/main/java/sdf01/nus/Main.java
@@ -2,5 +2,17 @@ package sdf01.nus;
 
 public class Main {
     public static void main(String[] args) {
+        int port;
+        String[] docRoot;
+
+        // parse args
+        ParsedArgs parsedArgs = new ParsedArgs(args);
+
+        port = parsedArgs.getPort();
+        docRoot = parsedArgs.getDocRoot();
+
+        // init server instance
+        HttpServer server = new HttpServer(port, docRoot);
+        server.start();
     }
 }
