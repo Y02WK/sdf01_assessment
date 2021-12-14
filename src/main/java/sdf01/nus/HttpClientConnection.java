4124e61bfe2d749802a8d14fa47497b556c566c6
Y02WK <59220086+Y02WK@users.noreply.github.com>
2021-12-14 13:11:38 +0800

    Added write methods for 405 and 404.

--- src/main/java/sdf01/nus/HttpClientConnection.java
+++ src/main/java/sdf01/nus/HttpClientConnection.java
@@ -4,22 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Optional;
 
 public class HttpClientConnection implements Runnable {
     private Socket socket;
     private HttpWriter writer;
     private BufferedReader reader;
-    // new requestparser
-    // FileHandling
+    private FilesHandler fileHandler;
 
-    public HttpClientConnection(Socket socket) {
+    public HttpClientConnection(Socket socket, FilesHandler fileHandler) {
         this.socket = socket;
+        this.fileHandler = fileHandler;
     }
 
     public void run() {
         initStreams();
         try {
-            String request = receiveFromBrowser();
+            parseRequest(receiveFromBrowser());
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -53,15 +55,46 @@ public class HttpClientConnection implements Runnable {
         String url = requestArgs[1];
 
         if (!method.equals("GET")) {
-            return;
+            this.write405(method);
         }
 
         // check if File exists
+        Optional<String> fileDir = fileHandler.fileExists(url);
+        if (!fileDir.isPresent()) {
+            this.write404(url);
+        } else {
+            // 200 OK
+        }
+
         // goes through each dir and checks if the file exists
 
         // check PNG
     }
 
-    // Send to browser using HttpWriter
+    private void write405(String method) {
+        String response = "HTTP/1.1 405 Method Not Allowed";
+        String resText = method + " not supported";
+        try {
+            writer.writeString(response);
+            writer.writeString();
+            writer.writeString(resText);
+            writer.close();
+        } catch (Exception e) {
+            System.err.println("Error in sending response to browser");
+        }
+    }
+
+    private void write404(String url) {
+        String response = "HTTP/1.1 404 Not Found";
+        String resText = url + " not found";
+        try {
+            writer.writeString(response);
+            writer.writeString();
+            writer.writeString(resText);
+            writer.close();
+        } catch (Exception e) {
+            System.err.println("Error in sending response to browser");
+        }
+    }
 
 }
