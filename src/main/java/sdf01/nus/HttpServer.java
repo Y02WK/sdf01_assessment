cb72345f508b86623e6f88b45a1fcbf54bbff423
Y02WK <59220086+Y02WK@users.noreply.github.com>
2021-12-14 10:48:28 +0800

    Implemented checkDocRoot() method.

--- src/main/java/sdf01/nus/HttpServer.java
+++ src/main/java/sdf01/nus/HttpServer.java
@@ -1,10 +1,15 @@
 package sdf01.nus;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class HttpServer {
+    private int port;
+    private String[] docRoot;
     private ServerSocket serverSocket;
     private ExecutorService threadPool;
 
@@ -13,6 +18,8 @@ public class HttpServer {
 
     // constructor
     public HttpServer(int port, String[] docRoot) {
+        this.port = port;
+        this.docRoot = docRoot;
         // init serverSocket
 
         // start serverSocket
@@ -20,8 +27,20 @@ public class HttpServer {
         // init threadPool
     }
 
+    public void start() {
+        this.initServer();
+        this.startServer();
+        return;
+    }
+
     private void initServer() {
-        this.serverSocket = new ServerSocket(port);
+        // try to start serverSocket on the port. exits if failed
+        try {
+            this.serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.err.println("Failed to start serverSocket on port " + port);
+            System.exit(1);
+        }
         this.threadPool = Executors.newFixedThreadPool(3);
 
         // check if connected
@@ -36,4 +55,18 @@ public class HttpServer {
         // submit thread to the threadpool
     }
 
+    private boolean checkDocRoot() {
+        // if checks pass return true
+        // else return false
+
+        // check if exists + check if directory (Files.isDirectory())
+        // check if readable (Files.isReadable())
+        for (String dir : docRoot) {
+            Path dirPath = Path.of(dir);
+            if (!Files.isReadable(dirPath) || !Files.isDirectory(dirPath)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
