65594df4074db38220276f9d16946b6b43d1b0a2
Y02WK <59220086+Y02WK@users.noreply.github.com>
2021-12-17 22:31:01 +0800

    Refactor code

--- src/main/java/sdf01/nus/ServerConsole.java
+++ src/main/java/sdf01/nus/ServerConsole.java
@@ -16,12 +16,12 @@ public class ServerConsole {
     public ServerConsole(int port, String[] docRoot) {
         // single thread executor for one instance of the server
         threadPool = Executors.newSingleThreadExecutor();
-        this.serverInstance = new HttpServer(port, docRoot);
+        serverInstance = new HttpServer(port, docRoot);
     }
 
     // run server console
     public void run() {
-        System.out.println("Enter 'start' to start the server.");
+        System.out.println("Enter 'start' to start the server, or 'quit' to exit");
         while (true) {
             String cmd = "";
             try {
@@ -41,34 +41,44 @@ public class ServerConsole {
         return reader.readLine();
     }
 
+    // process user input from console
     private boolean processInput(String cmd) {
         switch (cmd.trim().toLowerCase()) {
             case "start":
                 runServer();
                 return true;
             case "stop":
-                if (serverRunning) {
-                    stopServer();
-                    return false;
-                } else {
-                    System.out.println("Server not started. Enter 'start' to start server.");
-                    return true;
-                }
+                stopServer();
+                return true;
+            case "quit":
+                quit();
+                return false;
             default:
-                System.out.println("Command not recognized. 'start' or 'stop' only");
+                System.out.println("Command not recognized. 'start'/'stop'/'quit' only");
                 return true;
         }
     }
 
     // init server instance
     private void runServer() {
-        this.serverFuture = threadPool.submit(serverInstance);
+        serverFuture = threadPool.submit(serverInstance);
         System.out.println("Server started. Enter 'stop' to stop server.");
     }
 
-    // stops the server instance immediately
+    // stops the server instance immediately if server is running
     private void stopServer() {
-        this.serverFuture.cancel(true);
-        System.out.println("Server stopped");
+        if (serverRunning) {
+            serverFuture.cancel(true);
+            System.out.println("Server stopped");
+        } else {
+            System.out.println("Server not started. Enter 'start' to start server.");
+        }
+    }
+
+    // quit and shutdown the program
+    private void quit() {
+        stopServer();
+        threadPool.shutdown();
+        System.out.println("Quit. Byebye");
     }
 }
