f57cdd728c958c9440aa62ead987a0be19d6582f
Y02WK <59220086+Y02WK@users.noreply.github.com>
2021-12-17 20:47:52 +0800

    Removed redundant code

--- src/main/java/sdf01/nus/Main.java
+++ src/main/java/sdf01/nus/Main.java
@@ -11,10 +11,6 @@ public class Main {
         port = parsedArgs.getPort();
         docRoot = parsedArgs.getDocRoot();
 
-        // init server instance
-        // HttpServer server = new HttpServer(port, docRoot);
-        // server.run();
-
         // init server console
         ServerConsole console = new ServerConsole(port, docRoot);
         console.run();
