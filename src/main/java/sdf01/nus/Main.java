c4d292af3c92ffc9ca3efc62a0694032c5a7191c
Y02WK <59220086+Y02WK@users.noreply.github.com>
2021-12-17 20:47:39 +0800

    Starts from ServerConsole instead

--- src/main/java/sdf01/nus/Main.java
+++ src/main/java/sdf01/nus/Main.java
@@ -12,7 +12,11 @@ public class Main {
         docRoot = parsedArgs.getDocRoot();
 
         // init server instance
-        HttpServer server = new HttpServer(port, docRoot);
-        server.start();
+        // HttpServer server = new HttpServer(port, docRoot);
+        // server.run();
+
+        // init server console
+        ServerConsole console = new ServerConsole(port, docRoot);
+        console.run();
     }
 }
